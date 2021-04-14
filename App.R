109c5b83cfa00165bdb213e79f7cd826621fa2b7
sjpeters3 <61508055+sjpeters3@users.noreply.github.com>
2021-04-14 13:57:54 -0400

    Moved the "all" data to its own plot. Added a legend to the original plot that is now used to select groups to display.

--- App.R
+++ App.R
@@ -13,47 +13,61 @@ taxonomic_security = read_csv("./data/taxonomic_security.csv") %>%
   arrange(desc(Num_Species)) %>% 
   mutate_at(vars(Subgroup), list(~factor(., levels=unique(.))))
 
-# Get a list of all the taxonomic subgroups
-taxonomic_subgroups = taxonomic_security %>% distinct(Subgroup)
-
 # Plot on top with the inputs on the bottom
 ui <- fluidPage(
-  
-  splitLayout(
     
-    plotlyOutput(outputId = "stateSecurePlot"),
-    
-    checkboxGroupInput(
-      inputId = "tax_subgroup",
-      label = "Taxonomic SubGroup",
-      choices = taxonomic_subgroups[[1]],
-      selected = taxonomic_subgroups[[1]][1],
-      inline = FALSE
-    )
-    
-  ),
-  
+  plotlyOutput(outputId = "stateSecurePlot"),
+
   mainPanel(
     p("This plot aims to show the correlation between the number of species in a county, 
-      and the percentage of those species that are not secure. Taxonomic subgroups  are 
+      and the percentage of those species that are unsecure. Taxonomic subgroups are 
       selectable and control which data is plotted. Each point represents a county. The 
       x-value is the number of species belonging to that taxonomic subgroup that are found 
       in that county. The y-value is the percentage of species in that taxonomic subgroup 
-      and county that are not secure at a state level. Color corresponds to the taxonomic 
-      subgroup, which is shown on mouseover. The \"All\" selection displays values combined 
-      for all taxonomic subgroups, even those which are not available for selection here."
-      )
+      and county that are unsecure at a state level. Color corresponds to the taxonomic 
+      subgroup, which is shown on mouseover."
+      ),
+    plotlyOutput(outputId = "allSecurePlot"),
+    p("The above graph is the same as the previous graph, except that information is plotted
+      across all taxonomic subgroups, including those which were previously not available 
+      for selection.")
   )
+  
+  
   
 )
 
 server <- function(input, output) {
   
-  # Create the scatterplot of number of species vs. percent not secure state
+  # Create the scatterplot of number of species vs. percent unsecure state
   output$stateSecurePlot <- renderPlotly({
     
     plot = taxonomic_security %>% 
-      filter(Subgroup %in% input$tax_subgroup) %>% 
+      filter(Subgroup != "All") %>% 
+      ggplot(aes(x = Num_Species, y = `%Unsecure`, name = County, col = Subgroup)) + 
+      geom_jitter(alpha = 0.5, size = 1, width = 0.3, height = 0.3) +
+      theme_bw() +
+      theme(
+        panel.background = element_rect(fill = "#f7f7f7")#,
+        #legend.position="none"
+      ) +
+      labs (
+        title = "Percent of Species Unsecure",
+        x = "Number of Species of Subgroup",
+        y = "% Unsecure of Subgroup"
+      )
+    
+    # Remove ability to pan and zoom
+    ggplotly(plot) %>% 
+      config(displayModeBar = FALSE) %>% 
+      layout(xaxis=list(fixedrange=TRUE),
+             yaxis=list(fixedrange=TRUE))
+  })
+  
+  output$allSecurePlot <- renderPlotly({
+    
+    plot = taxonomic_security %>% 
+      filter(Subgroup == "All") %>% 
       ggplot(aes(x = Num_Species, y = `%Unsecure`, name = County, col = Subgroup)) + 
       geom_jitter(alpha = 0.5, size = 1, width = 0.3, height = 0.3) +
       theme_bw() +
@@ -62,13 +76,13 @@ server <- function(input, output) {
         legend.position="none"
       ) +
       labs (
-        title = "Percent of Species not Secure",
-        x = "Number of Species of Subgroup",
-        y = "% Not Secure of Subgroup"
+        title = "Percent of Species Unecure",
+        x = "Number of Species of Birds",
+        y = "% Unsecure of all Birds"
       )
     
     # Remove ability to pan and zoom
-    ggplotly(plot) %>% 
+    ggplotly(plot, width = 500) %>% 
       config(displayModeBar = FALSE) %>% 
       layout(xaxis=list(fixedrange=TRUE),
              yaxis=list(fixedrange=TRUE))
