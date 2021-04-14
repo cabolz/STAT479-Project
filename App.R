48e8a1fc35e916fb7095d96c98b290fbf7914ca5
sjpeters3 <61508055+sjpeters3@users.noreply.github.com>
2021-04-14 18:00:36 -0400

    Cleaned the plots. Renamed columns of their datasets.

--- App.R
+++ App.R
@@ -7,16 +7,12 @@ library("RColorBrewer")
 
 # Load in dataset, ordering by largest taxonomic subgroup
 taxonomic_security = read_csv("./data/taxonomic_security.csv") %>% 
-  rename(Num_Species = n,
-         Subgroup = taxonomic_subgroup,
-         County = county,
-         `%Unsecure` = perc_not_secure_state) %>% 
-  arrange(desc(Num_Species)) %>% 
+  arrange(desc(Species)) %>% 
   mutate_at(vars(Subgroup), list(~factor(., levels=unique(.))))
 
 species_by_subgroup = read_csv("./data/species_by_subgroup.csv") %>% 
-  arrange(desc(n_species)) %>% 
-  mutate_at(vars(taxonomic_subgroup), list(~factor(., levels=unique(.))))
+  arrange(desc(Species)) %>% 
+  mutate_at(vars(Subgroup), list(~factor(., levels=unique(.))))
 
 choropleth = read_csv("./data/choropleth.csv")
 
@@ -29,11 +25,11 @@ ui <- fluidPage(
     
     mainPanel(
       p("This plot aims to show the correlation between the number of species in a county, 
-        and the percentage of those species that are unsecure. Taxonomic subgroups are 
+        and the percentage of those species that are not secure Taxonomic subgroups are 
         selectable and control which data is plotted. Each point represents a county. The 
         x-value is the number of species belonging to that taxonomic subgroup that are found 
         in that county. The y-value is the percentage of species in that taxonomic subgroup 
-        and county that are unsecure at a state level. Color corresponds to the taxonomic 
+        and county that are not secure at a state level. Color corresponds to the taxonomic 
         subgroup, which is shown on mouseover."
         )
     ),
@@ -57,43 +53,42 @@ ui <- fluidPage(
     HTML("<br><br><br>"), 
     
     plotlyOutput(outputId = "subgroupPlot")
-    
   
 )
 
 server <- function(input, output) {
   
-  # Create the scatterplot of number of species vs. percent unsecure state
+  # Create the scatterplot of number of species vs. percent not secure state by taxonomic subgroup
   output$stateSecurePlot <- renderPlotly({
     
     plot = taxonomic_security %>% 
       filter(Subgroup != "All") %>% 
-      ggplot(aes(x = Num_Species, y = `%Unsecure`, name = County, col = Subgroup)) + 
+      ggplot(aes(x = Species, y = `%Not Secure`, name = County, col = Subgroup)) + 
       geom_jitter(alpha = 0.5, size = 1, width = 0.3, height = 0.3) +
       theme_bw() +
       theme(
-        panel.background = element_rect(fill = "#f7f7f7")#,
-        #legend.position="none"
+        panel.background = element_rect(fill = "#f7f7f7")
       ) +
       labs (
-        title = "Percent of Species Unsecure",
+        title = "Percent of Species Not Secure",
         x = "Number of Species of Subgroup",
-        y = "% Unsecure of Subgroup"
+        y = "% Not Secure of Subgroup"
       ) +
-        scale_color_manual(values = colorRampPalette(brewer.pal(8, "Set1"))(12))
+      scale_color_manual(values = colorRampPalette(brewer.pal(8, "Set1"))(12))
     
-    # Remove ability to pan and zoom
+    # Remove ability to pan and zoom, set plot dimensions
     ggplotly(plot, width = 800, height = 450) %>% 
       config(displayModeBar = FALSE) %>% 
       layout(xaxis=list(fixedrange=TRUE),
              yaxis=list(fixedrange=TRUE))
   })
   
+  # Create the scatterplot of number of species vs. percent not secure state with all subgroups combined
   output$allSecurePlot <- renderPlotly({
     
     plot = taxonomic_security %>% 
       filter(Subgroup == "All") %>% 
-      ggplot(aes(x = Num_Species, y = `%Unsecure`, name = County, col = Subgroup)) + 
+      ggplot(aes(x = Species, y = `%Not Secure`, name = County, col = Subgroup)) + 
       geom_jitter(alpha = 0.5, size = 1, width = 0.3, height = 0.3) +
       theme_bw() +
       theme(
@@ -101,48 +96,63 @@ server <- function(input, output) {
         legend.position="none"
       ) +
       labs (
-        title = "Percent of Species Unecure",
+        title = "Percent of Species Not Secure",
         x = "Number of Species of Birds",
-        y = "% Unsecure of all Birds"
+        y = "% Not Secure of all Birds"
       )
     
-    # Remove ability to pan and zoom
+    # Remove ability to pan and zoom, set plot dimensions
     ggplotly(plot, width = 500) %>% 
       config(displayModeBar = FALSE) %>% 
       layout(xaxis=list(fixedrange=TRUE),
              yaxis=list(fixedrange=TRUE))
   })
   
+  # Bar plot of the number of species for each taxonomic subgroup
   output$subgroupPlot <- renderPlotly({
     
-    plot = ggplot(species_by_subgroup, aes(x=n_species, y=taxonomic_subgroup)) +
+    plot = ggplot(species_by_subgroup, aes(x= Species, y = Subgroup, fill = Subgroup)) +
       geom_col() +
-      xlab("Number of Species") +
-      ylab("Taxonomic Subgroup") +
-      theme(axis.text.y = element_text(size=7))
+      labs(
+        x = "Number of Species",
+        y = "Taxonomic Subgroup"
+      ) +
+      theme_minimal() +
+      theme(
+        axis.text.y = element_text(size=7),
+        panel.grid.major.y = element_blank(),
+        legend.position="none"
+      ) +
+      scale_x_continuous(breaks = seq(from = 0, to = 35, by = 5))
     
-    # Remove ability to pan and zoom
-    ggplotly(plot, width = 700) %>% 
+    # Remove ability to pan and zoom, set plot dimensions
+    ggplotly(plot, width = 700, tooltip = c("x", "y")) %>% 
       config(displayModeBar = FALSE) %>% 
       layout(xaxis=list(fixedrange=TRUE),
              yaxis=list(fixedrange=TRUE))
   })
   
-  # Create the scatterplot of number of species vs. percent unsecure state
+  # Choropleth map of NY counties, filling by the number of species
   output$countyChoropleth <- renderPlotly({
     
     plot = ggplot(choropleth, aes(long, lat, group = County)) +
       geom_polygon(aes(fill = Species), colour = alpha("black", 1/2), size = 0.1)  +
-      scale_fill_viridis_c() +
-      theme_void() +
-      theme(panel.grid = element_blank()) +
       labs (
         title = "Number of Species by County",
-        fill = "County"
-      )
+        fill = "Number of Species"
+      ) +
+      theme(
+        panel.background = element_rect(fill = "#FFFFFF"),
+        panel.grid = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()
+      ) +
+      scale_fill_viridis_c(limits = c(50, 200))
     
-    # Remove ability to pan and zoom
-    ggplotly(plot, width = 600) %>% 
+    # Remove ability to pan and zoom, set plot dimensions
+    ggplotly(plot, width = 700) %>% 
       config(displayModeBar = FALSE) %>% 
       layout(xaxis=list(fixedrange=TRUE),
              yaxis=list(fixedrange=TRUE))
