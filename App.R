a45380cd3226f61aaa0fd1e292f64b7330e997ee
sjpeters3 <61508055+sjpeters3@users.noreply.github.com>
2021-05-01 22:12:38 -0400

    Implemented all of the instructor feedback relating to the plots.

--- App.R
+++ App.R
@@ -20,7 +20,8 @@ species_by_subgroup = read_csv("./data/species_by_subgroup.csv") %>%
   arrange(desc(Species)) %>% 
   mutate_at(vars(Subgroup), list(~factor(., levels=unique(.))))
 
-choropleth = read_csv("./data/choropleth.csv")
+choropleth = read_csv("./data/choropleth.csv") %>% 
+  rename(`Density (pop/mi^2)` = "Density")
 
 # Plot on top with the inputs on the bottom
 ui <- fluidPage(
@@ -73,7 +74,7 @@ ui <- fluidPage(
     
   plotlyOutput(outputId = "subgroupPlot"),
   
-  HTML("<br>"),
+  HTML("<br><br><br><br><br><br><br><br><br><br><br>"),
     
   mainPanel("This graph provides information on the number of species found in New York for 
         each taxonomic subgroup. The bars show the subgroup along the y-axis and the 
@@ -151,16 +152,16 @@ server <- function(input, output) {
     
     plot = taxonomic_security %>% 
       filter(Subgroup != "All") %>% 
-      ggplot(aes(x = Species, y = `%Not Secure`, name = County, col = Subgroup)) + 
-      geom_jitter(alpha = 0.5, size = 1, width = 0.3, height = 0.3) +
+      ggplot(aes(x = Species, y = `% Not Secure`)) + 
+      geom_jitter(aes(col = Subgroup, name = County), alpha = 0.5, size = 1, width = 0.3, height = 0.3) +
       theme_bw() +
       theme(
         panel.background = element_rect(fill = "#f7f7f7")
       ) +
       labs (
         title = "Percent of Species Not Secure",
-        x = "Number of Species of Subgroup",
-        y = "% Not Secure of Subgroup"
+        x = "Number of Species",
+        y = "% Not Secure"
       ) +
       scale_color_manual(values = colorRampPalette(brewer.pal(8, "Set1"))(12))
     
@@ -176,8 +177,8 @@ server <- function(input, output) {
     
     plot = taxonomic_security %>% 
       filter(Subgroup == "All") %>% 
-      ggplot(aes(x = Species, y = `%Not Secure`, name = County, col = Subgroup)) + 
-      geom_jitter(alpha = 0.5, size = 1, width = 0.3, height = 0.3) +
+      ggplot(aes(x = Species, y = `% Not Secure`)) + 
+      geom_jitter(aes(name = County), alpha = 0.75, size = 1, width = 0.3, height = 0.3) +
       theme_bw() +
       theme(
         panel.background = element_rect(fill = "#f7f7f7"),
@@ -185,8 +186,8 @@ server <- function(input, output) {
       ) +
       labs (
         title = "Percent of Species Not Secure by County",
-        x = "Number of Species of Birds",
-        y = "% Not Secure of all Birds"
+        x = "Number of Species",
+        y = "% Not Secure"
       )
     
     # Remove ability to pan and zoom, set plot dimensions
@@ -199,7 +200,7 @@ server <- function(input, output) {
   # Choropleth map of NY counties, filling by the number of species
   output$countyChoropleth <- renderPlotly({
     
-    plot = ggplot(choropleth, aes(long, lat, group = County, name = Density)) +
+    plot = ggplot(choropleth, aes(long, lat, group = County, name = `Density (pop/mi^2)`)) +
       geom_polygon(aes(fill = Species), colour = alpha("black", 1/2), size = 0.1)  +
       labs (
         title = "Number of Species by County",
@@ -216,7 +217,7 @@ server <- function(input, output) {
       scale_fill_viridis_c(option = "magma", limits = c(50, 200))
     
     # Remove ability to pan and zoom, set plot dimensions
-    ggplotly(plot, width = 700, tooltip = c("County", "Species", "Density")) %>% 
+    ggplotly(plot, width = 700, tooltip = c("County", "Species", "Density (pop/mi^2)")) %>% 
       config(displayModeBar = FALSE) %>% 
       layout(xaxis=list(fixedrange=TRUE),
              yaxis=list(fixedrange=TRUE))
@@ -225,23 +226,24 @@ server <- function(input, output) {
   # Bar plot of the number of species for each taxonomic subgroup
   output$subgroupPlot <- renderPlotly({
     
-    plot = ggplot(species_by_subgroup, aes(x= Species, y = Subgroup, fill = Subgroup)) +
+    plot = ggplot(species_by_subgroup, aes(x= Species, y = Subgroup, fill = "#FF0000")) +
       geom_col() +
       labs(
         x = "Number of Species",
         y = "Taxonomic Subgroup",
         title = "Number of Species in each Subgroup"
       ) +
-      theme_minimal() +
+      theme_bw() +
       theme(
-        axis.text.y = element_text(size=7),
+        axis.text.y = element_text(size=9),
         panel.grid.major.y = element_blank(),
-        legend.position="none"
+        legend.position="none",
+        panel.background = element_rect(fill = "#f7f7f7")
       ) +
       scale_x_continuous(breaks = seq(from = 0, to = 35, by = 5))
     
     # Remove ability to pan and zoom, set plot dimensions
-    ggplotly(plot, width = 700, tooltip = c("x", "y")) %>% 
+    ggplotly(plot, width = 700, height = 600, tooltip = c("x", "y")) %>% 
       config(displayModeBar = FALSE) %>% 
       layout(xaxis=list(fixedrange=TRUE),
              yaxis=list(fixedrange=TRUE))
